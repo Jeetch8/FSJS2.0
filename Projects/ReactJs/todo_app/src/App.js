81c53fa0aa94366550649d1a9570cb5b9c8c3bfb
Jeet Chawda <jeetkumar0898@gmail.com>
2023-02-26 22:57:13 +0530

    func added

--- Projects/ReactJs/todo_app/src/App.js
+++ Projects/ReactJs/todo_app/src/App.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import SingleItem from "./Components/SingleItem";
+import uuid from "react-uuid";
 
 function App() {
   const [list, setList] = useState([]);
+  const [taskCompletedList, setTaskCompletedList] = useState([]);
+  const [taskInCompleteList, setTaskIncomplete] = useState([]);
   const [inputVal, setInputVal] = useState("");
+  const [taskStatus, setTaskStatus] = useState("InComplete");
 
   const backgroundList = [
     "https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg",
@@ -17,14 +21,39 @@ function App() {
   // ?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=50&w=60
 
   const addItemToList = () => {
-    setList([...list, inputVal]);
+    const itemId = uuid();
+    const itemObj = { title: inputVal, checked: false, itemId };
+    setTaskIncomplete([...taskInCompleteList, itemObj]);
+    if (taskStatus === "InComplete") {
+      setList([...list, itemObj]);
+    } else if (taskStatus === "Completed") {
+      setTaskStatus("InComplete");
+      setList([...taskInCompleteList, itemObj]);
+    }
     setInputVal("");
   };
 
-  const deleteItem = (index) => {
-    const newArr = [...list];
-    newArr.splice(index, 1);
-    setList(newArr);
+  const deleteItem = (itemId) => {
+    const itemIndex =
+      taskStatus === "Completed"
+        ? taskCompletedList.findIndex((item) => {
+            return itemId === item.itemId;
+          })
+        : taskInCompleteList.findIndex((item) => {
+            return itemId === item.itemId;
+          });
+
+    const listCopy = [...list];
+    const listItemIndex = listCopy.findIndex((item) => {
+      return itemId === item.itemId;
+    });
+    if (taskStatus === "Completed" && itemIndex !== -1) {
+      taskCompletedList.splice(itemIndex, 1);
+    } else if (taskStatus === "InComplete" && itemIndex !== -1) {
+      taskInCompleteList.splice(itemIndex, 1);
+    }
+    listCopy.splice(listItemIndex, 1);
+    setList([...listCopy]);
   };
 
   const changeItemTitle = (index, val) => {
@@ -33,38 +62,131 @@ function App() {
     setList([...newArr]);
   };
 
+  const changeTaskStatus = ({ itemId, title, status }) => {
+    if (status) {
+      const taskInCompIndex = taskInCompleteList.findIndex((item) => {
+        return item.itemId === itemId;
+      });
+      const listIndex = list.findIndex((item) => {
+        return item.itemId === itemId;
+      });
+      setTaskCompletedList([
+        ...taskCompletedList,
+        { itemId, title, checked: true },
+      ]);
+      taskInCompleteList.splice(taskInCompIndex, 1);
+      list.splice(listIndex, 1);
+    } else if (!status) {
+      const taskCompIndex = taskCompletedList.findIndex((item) => {
+        return item.itemId === itemId;
+      });
+      const listIndex = list.findIndex((item) => {
+        return item.itemId === itemId;
+      });
+      setTaskIncomplete([
+        ...taskInCompleteList,
+        { itemId, title, checked: false },
+      ]);
+      taskCompletedList.splice(taskCompIndex, 1);
+      list.splice(listIndex, 1);
+    }
+  };
+
+  const newTaskStatusVisible = (status) => {
+    if (status === "Completed") {
+      setList([...taskCompletedList]);
+      setTaskStatus("Completed");
+    } else if (status === "InComplete") {
+      setList([...taskInCompleteList]);
+      setTaskStatus("InComplete");
+    }
+  };
+
   return (
-    <div style={{ backgroundImage: `url(${backgroundList[0]})` }}>
-      <div>
-        <input
-          className="border-2 border-black"
-          type="text"
-          value={inputVal}
-          onKeyDown={(e) => (e.key === "Enter" ? addItemToList() : null)}
-          onChange={(e) => setInputVal(e.target.value)}
-        />
-        <button
-          className="border-2 border-black"
-          onClick={() => addItemToList()}
-        >
-          Add
-        </button>
-      </div>
-      <div>
-        {list.map((item, i) => {
-          return (
-            <SingleItem
-              key={i}
-              item={item}
-              i={i}
-              deleteItem={deleteItem}
-              changeItemTitle={changeItemTitle}
-            />
-          );
-        })}
+    <div
+      style={{ backgroundImage: `url(${backgroundList[0]})` }}
+      className="bg-cover h-[100vh] w-full text-white"
+    >
+      <div className="h-[80vh] my-auto bg-[rgba(0,0,0,0.3)] w-[40vw] mx-auto overflow-y-scroll">
+        <div className="flex justify-center pt-10">
+          <input
+            className="border-2 border-black bg-transparent"
+            type="text"
+            value={inputVal}
+            onKeyDown={(e) => (e.key === "Enter" ? addItemToList() : null)}
+            onChange={(e) => setInputVal(e.target.value)}
+          />
+          <button
+            className="border-2 border-black"
+            onClick={() => addItemToList()}
+          >
+            Add
+          </button>
+        </div>
+        <div className="flex justify-center gap-4 my-4">
+          <button
+            onClick={() => newTaskStatusVisible("InComplete")}
+            className={
+              taskStatus === "InComplete"
+                ? "border-[1px] rounded-full text-black cursor-pointer font-medium px-6 py-2 bg-[rgba(238,238,238,0.7)]"
+                : "border-[1px] border-black rounded-full px-6 py-2 cursor-pointer font-medium"
+            }
+          >
+            Task Not Completed
+          </button>
+          <button
+            onClick={() => newTaskStatusVisible("Completed")}
+            className={
+              taskStatus === "Completed"
+                ? "border-[1px] rounded-full text-black font-medium px-6 py-2 bg-[rgba(238,238,238,0.7)] cursor-pointer"
+                : "border-[1px] border-black cursor-pointer rounded-full px-6 py-2 font-medium"
+            }
+          >
+            Task Completed
+          </button>
+        </div>
+        <div>
+          {list.map((item) => {
+            return (
+              <SingleItem
+                key={item.itemId}
+                item={item}
+                deleteItem={deleteItem}
+                changeItemTitle={changeItemTitle}
+                changeTaskStatus={changeTaskStatus}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
+// const changeTaskStatusVisible = (status) => {
+//   const localArr = [...document.querySelectorAll("input")];
+//   const checkBoxArr = [];
+//   const newArr = [];
+//   localArr.forEach((el) => {
+//     if (el.type === "checkbox") {
+//       checkBoxArr.push(el);
+//     }
+//   });
+//   if (status === "Completed") {
+//     setTaskStatus("Completed");
+//   } else if (status === "NotCompleted") {
+//     setTaskStatus("NotCompleted");
+//   }
+//   if (status === "Completed") {
+//     checkBoxArr.forEach((item) => {
+//       if (item.checked) newArr.push(item);
+//     });
+//   } else if (status === "NotCompleted") {
+//     checkBoxArr.forEach((item) => {
+//       if (!item.checked) newArr.push(item);
+//     });
+//   }
+//   setList([...newArr]);
+// };
--- Projects/ReactJs/todo_app/src/Components/SingleItem.js
+++ Projects/ReactJs/todo_app/src/Components/SingleItem.js
@@ -3,42 +3,88 @@ import { TbEdit } from "react-icons/tb";
 import { MdDeleteForever } from "react-icons/md";
 import { TiTick } from "react-icons/ti";
 
-const SingleItem = ({ i, item, deleteItem, changeItemTitle }) => {
+const SingleItem = ({
+  item,
+  deleteItem,
+  changeItemTitle,
+  changeTaskStatus,
+}) => {
   const [inputVis, setInputVis] = useState(false);
   const [newTitle, setNewTitle] = useState("");
-  const [taskComp, setTaskComp] = useState(false);
   const handlenewTitleChangeFunc = () => {
-    changeItemTitle(i, newTitle);
+    changeItemTitle(item.itemId, newTitle);
     setInputVis(false);
   };
+
   return (
-    <div key={i} className="flex">
-      <input type="checkbox" onChange={() => setTaskComp(!taskComp)} />
-      {inputVis ? (
-        <div>
+    <div className="flex border-2 border-gray-500 px-6 py-2 w-fit mx-auto my-4 bg-[rgba(0,0,0,0.5)] gap-4 text-white items-center">
+      <input
+        type="checkbox"
+        className="h-[20px] w-[20px]"
+        onChange={(e) =>
+          changeTaskStatus({
+            itemId: item.itemId,
+            status: e.target.checked,
+            title: item.title,
+          })
+        }
+        defaultChecked={item.checked}
+      />
+      <div
+        className={
+          inputVis
+            ? "outline-1 outline-white outline flex items-center gap-2"
+            : "flex items-center gap-2"
+        }
+      >
+        {item.checked ? (
           <input
             type="text"
             onKeyDown={(e) =>
               e.key === "Enter" ? handlenewTitleChangeFunc() : null
             }
-            value={newTitle === "" ? item : newTitle}
+            defaultValue={item.title}
             onChange={(e) => setNewTitle(e.target.value)}
+            className={
+              inputVis
+                ? "px-4 bg-transparent py-2 outline-none line-through"
+                : "bg-transparent border-none outline-none px-4 py-2 line-through"
+            }
+            readOnly={!inputVis}
+            onClick={() => setInputVis(true)}
           />
-          <button>
-            <TiTick onClick={() => handlenewTitleChangeFunc()} />
+        ) : (
+          <input
+            type="text"
+            onKeyDown={(e) =>
+              e.key === "Enter" ? handlenewTitleChangeFunc() : null
+            }
+            defaultValue={item.title}
+            onChange={(e) => setNewTitle(e.target.value)}
+            className={
+              inputVis
+                ? "px-4 bg-transparent py-2 outline-none"
+                : "bg-transparent border-none outline-none px-4 py-2"
+            }
+            readOnly={!inputVis}
+            onClick={() => setInputVis(true)}
+          />
+        )}
+        {inputVis && (
+          <button onClick={() => handlenewTitleChangeFunc()} className="px-2">
+            <TiTick />
           </button>
-        </div>
-      ) : (
-        <div
-          className={taskComp ? "line-through" : null}
-          onClick={() => setInputVis(true)}
-        >
-          {item}
-        </div>
-      )}
-      <div className="flex">
-        <MdDeleteForever onClick={() => deleteItem(i)} />
-        <TbEdit onClick={() => setInputVis(!inputVis)} />
+        )}
+      </div>
+      <div className="flex gap-2 text-[20px]">
+        <MdDeleteForever
+          onClick={() => deleteItem(item.itemId)}
+          className="cursor-pointer"
+        />
+        <TbEdit
+          onClick={() => setInputVis(!inputVis)}
+          className="cursor-pointer"
+        />
       </div>
     </div>
   );
